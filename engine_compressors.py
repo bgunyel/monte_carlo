47c7eb1baa1be955b46e1a00280b25e5d4c7c47c
Bertan Gunyel <bertan.gunyel@gmail.com>
2021-04-20 15:31:42 +0300

    Runtime: 1.15 sec/scenario

--- engine_compressors.py
+++ engine_compressors.py
@@ -11,46 +11,48 @@ def main(name):
     nValues = [2, 3, 4, 5, 6, 7]
     numberOfIterations = 1000
     numberOfDays = 100
-
     probabilityOfPartFailure = {1: 0.02, 2: 0.05, 3: 0.10, 4: 0.12, 5: 0.15, 6: 0.20, 7: 0.25}
-
     poissonMean = 8.2
     costPerCompressorPerDay = 80
     partReplacementCost = 500
-
     queueCrowdLimit = 15
 
     outputFolder = '/home/bgunyel/Output/monte_carlo/'
 
     startTime = time.time()
 
-    scenarioDf = pd.DataFrame(data={'Regular Replacement Duration': nValues,
-                                    'Average Total Cost': np.zeros(len(nValues)),
-                                    'Average Number of Compressors in Queue': np.zeros(len(nValues)),
-                                    'Number of Scheduled Replacements': np.zeros(len(nValues)),
-                                    'Number of Part Failures': np.zeros(len(nValues)),
-                                    'Probability of Crowded Queue': np.zeros(len(nValues))})
+    scenarioDf = {'Regular Replacement Duration': nValues,
+                  'Average Total Cost': np.zeros(len(nValues)),
+                  'StdDev Total Cost': np.zeros(len(nValues)),
+                  'Average Number of Compressors in Queue': np.zeros(len(nValues)),
+                  'StdDev Number of Compressors in Queue': np.zeros(len(nValues)),
+                  'Average Number of Scheduled Replacements': np.zeros(len(nValues)),
+                  'StdDev Number of Scheduled Replacements': np.zeros(len(nValues)),
+                  'Average Number of Part Failures': np.zeros(len(nValues)),
+                  'StdDev Number of Part Failures': np.zeros(len(nValues)),
+                  'Probability of Crowded Queue': np.zeros(len(nValues))}
+
+    iterationDf = {'Day of Use': np.zeros(numberOfDays),
+                   'Did Part Fail': - 1 * np.ones(numberOfDays),
+                   'Scheduled Replacement': - 1 * np.ones(numberOfDays),
+                   'Number of Compressors in Queue': np.zeros(numberOfDays),
+                   'Is Queue Crowded': np.zeros(numberOfDays)}
+
+    simDf = {'Total Cost': np.zeros(numberOfIterations),
+             'Average Number Of Compressors in Queue': np.zeros(numberOfIterations),
+             'Number of Scheduled Replacements': np.zeros(numberOfIterations),
+             'Number of Part Failures': np.zeros(numberOfIterations),
+             'Number of Crowded Days': np.zeros(numberOfIterations)}
 
     for idx, n in enumerate(nValues):
 
         scenarioStartTime = time.time()
-
-        simDf = pd.DataFrame(data={'Total Cost': np.zeros(numberOfIterations),
-                                   'Average Number Of Compressors in Queue': np.zeros(numberOfIterations),
-                                   'Number of Scheduled Replacements': np.zeros(numberOfIterations),
-                                   'Number of Part Failures': np.zeros(numberOfIterations),
-                                   'Number of Crowded Days': np.zeros(numberOfIterations)})
 
         for iteration in range(numberOfIterations):
 
             totalCost = 0
             numberOfCompressorsInQueue = 0
             dayOfUse = 0
-
-            df = pd.DataFrame(data={'Day of Use': np.zeros(numberOfDays),
-                                    'Did Part Fail': - 1 * np.ones(numberOfDays),
-                                    'Scheduled Replacement': - 1 * np.ones(numberOfDays),
-                                    'Number of Compressors in Queue': np.zeros(numberOfDays)})
 
             for day in range(numberOfDays):
                 dayOfUse += 1
@@ -59,8 +61,8 @@ def main(name):
                 didPartFail = np.random.binomial(1, probabilityOfPartFailure[min(dayOfUse, 7)])
                 scheduledReplacement = 0
 
-                df.loc[day, 'Day of Use'] = dayOfUse
-                df.loc[day, 'Did Part Fail'] = didPartFail
+                iterationDf['Day of Use'][day] = dayOfUse
+                iterationDf['Did Part Fail'][day] = didPartFail
 
                 numberOfRepairedCompressors = -1
                 if didPartFail:
@@ -79,29 +81,34 @@ def main(name):
                 numberOfCompressorsInQueue = numberOfCompressorsInQueue - numberOfRepairedCompressors
                 totalCost += numberOfCompressorsInQueue * costPerCompressorPerDay
 
-                df.loc[day, 'Scheduled Replacement'] = scheduledReplacement
-                df.loc[day, 'Number of Compressors in Queue'] = numberOfCompressorsInQueue
+                iterationDf['Scheduled Replacement'][day] = scheduledReplacement
+                iterationDf['Number of Compressors in Queue'][day] = numberOfCompressorsInQueue
 
             # One iteration (100-day) is complete
 
-            df['Is Queue Crowded'] = np.zeros((numberOfDays, 1))
-            df.loc[df['Number of Compressors in Queue'] >= queueCrowdLimit, 'Is Queue Crowded'] = 1
+            iterationDf['Is Queue Crowded'] = np.zeros((numberOfDays, 1))
+            iterationDf['Is Queue Crowded'][iterationDf['Number of Compressors in Queue'] >= queueCrowdLimit] = 1
 
-            simDf.loc[iteration, 'Total Cost'] = totalCost
-            simDf.loc[iteration, 'Average Number Of Compressors in Queue'] = df['Number of Compressors in Queue'].mean()
-            simDf.loc[iteration, 'Number of Scheduled Replacements'] = df['Scheduled Replacement'].sum()
-            simDf.loc[iteration, 'Number of Part Failures'] = df['Did Part Fail'].sum()
-            simDf.loc[iteration, 'Number of Crowded Days'] = df['Is Queue Crowded'].sum()
+            simDf['Total Cost'][iteration] = totalCost
+            simDf['Average Number Of Compressors in Queue'][iteration] = iterationDf[
+                'Number of Compressors in Queue'].mean()
+            simDf['Number of Scheduled Replacements'][iteration] = iterationDf['Scheduled Replacement'].sum()
+            simDf['Number of Part Failures'][iteration] = iterationDf['Did Part Fail'].sum()
+            simDf['Number of Crowded Days'][iteration] = iterationDf['Is Queue Crowded'].sum()
 
         # One scenario (1000-iteration) is complete
 
-        scenarioDf.loc[idx, 'Regular Replacement Duration'] = n
-        scenarioDf.loc[idx, 'Average Total Cost'] = simDf['Total Cost'].mean()
-        scenarioDf.loc[idx, 'Average Number of Compressors in Queue'] = simDf[
-            'Average Number Of Compressors in Queue'].mean()
-        scenarioDf.loc[idx, 'Number of Scheduled Replacements'] = simDf['Number of Scheduled Replacements'].mean()
-        scenarioDf.loc[idx, 'Number of Part Failures'] = simDf['Number of Part Failures'].mean()
-        scenarioDf.loc[idx, 'Probability of Crowded Queue'] = simDf['Number of Crowded Days'].mean() / numberOfDays
+        scenarioDf['Regular Replacement Duration'][idx] = n
+        scenarioDf['Average Total Cost'][idx] = simDf['Total Cost'].mean()
+        scenarioDf['StdDev Total Cost'][idx] = simDf['Total Cost'].std()
+        scenarioDf['Average Number of Compressors in Queue'][idx] = simDf['Average Number Of Compressors in Queue'].mean()
+        scenarioDf['StdDev Number of Compressors in Queue'][idx] = simDf[
+            'Average Number Of Compressors in Queue'].std()
+        scenarioDf['Average Number of Scheduled Replacements'][idx] = simDf['Number of Scheduled Replacements'].mean()
+        scenarioDf['StdDev Number of Scheduled Replacements'][idx] = simDf['Number of Scheduled Replacements'].std()
+        scenarioDf['Average Number of Part Failures'][idx] = simDf['Number of Part Failures'].mean()
+        scenarioDf['StdDev Number of Part Failures'][idx] = simDf['Number of Part Failures'].std()
+        scenarioDf['Probability of Crowded Queue'][idx] = simDf['Number of Crowded Days'].mean() / numberOfDays
 
         scenarioEndTime = time.time()
         print(f'Scenario {idx} took {scenarioEndTime - scenarioStartTime} seconds')
@@ -109,10 +116,11 @@ def main(name):
     endTime = time.time()
     print(f'Total Elapsed Time: {endTime - startTime} seconds')
 
+    scenarioTable = pd.DataFrame(data=scenarioDf)
     timeStr = datetime.datetime.now().strftime('%Y-%m-%d_%H:%M')
     xlsFilePath = os.path.join(outputFolder, 'engine_compressors_' + timeStr + '.xlsx')
     excelWriter = pd.ExcelWriter(xlsFilePath)
-    scenarioDf.to_excel(excelWriter, 'Scenarios')
+    scenarioTable.to_excel(excelWriter, 'Scenarios')
     excelWriter.save()
 
 
